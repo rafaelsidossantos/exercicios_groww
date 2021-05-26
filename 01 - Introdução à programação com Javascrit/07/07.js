57b0690e976e331b454c801d29ee681bd0a485e2
rafaelsidossantos <rafaelsidossantos@gmail.com>
2021-05-26 20:52:49 -0300

    exercícios 6, 7 e 8)

--- 01 - Introdução à programação com Javascrit/07/07.js
+++ 01 - Introdução à programação com Javascrit/07/07.js
@@ -12,73 +12,24 @@ calculaPar([0,1,5])
 retorno: par
 */
 
-<<<<<<< HEAD
-function calculaPar (lista) {
-    //var soma = 0
-    
-        //console.log ($listanumeros){
-        for (var i = 0; i < lista.length; i++){
-        var soma = soma + listanumeros[i] }
-            if (soma$2===0){
-                console.log(`A soma dos valores é par e igual a ${soma}`)}
-            else {
-                console.log(`a soma dos valores é impar e igual a ${soma}` )}
-                
-        }               
-             
-=======
-function calculaPar(lista){
-    var i = 0; //Variavel incrementadora que vai de 0 ~ tamanho da lista
-    var soma = 0; //Variavel auxiliar para somar todos os itens,
-    while(i < lista.length){
+function calculaPar(lista) {
+    var i = 0; 
+    var soma = 0; 
+    while (i < lista.length) {
         soma = soma + lista[i]
-        i++        
+        i++
     }
-    if( soma%2 === 0){
+    if (soma % 2 === 0) {
         console.log(`Número par ${soma}`)
-    }else{
+    } else {
         console.log(`Número Impar ${soma}`)
     }
 }
 
 
 
-function calculaParporfavo (lista) {
-var soma = 0
-    //console.log ($listanumeros){
-    for (var i = 0; i < lista.length; i++){
-    soma = lista[i] + soma
-        if (soma%2==0) {
-            console.log(`A soma dos valores é par e igual a ${soma}`)}
-        else {
-            console.log(`a soma dos valores é impar e igual a ${soma}` )}
-            
-    }
-}              
 
-function calcVetor (){
-    let calculaNumeros = [];
 
-    //Estrutura for para preencher um array
-    for (let i = 0; i < 10; i++){
-        calculaNumeros.push(prompt(`Informe o ${i+1}º número:`));
-    }
-
-    let soma = 0; 
-    for (let i = 0; i < calculaNumeros.length; i++){
-         soma += parseInt(calculaNumeros[i]);
-    }
-
-    if (soma % 2 === 0) {
-        return alert(`Par`);
-    } else {
-        if (soma % 2 !== 0){
-            return alert(`Ímpar`);
-        }
-    }
-}
-
->>>>>>> af8eab88ab06cf0008549c4d03a30eb7f2cada26
 
 /* Questão 02
 
@@ -91,7 +42,7 @@ CalculaExpoente(5,5)
 Saída: 3125
 
 */
-function expoente (n1 = 0, n2 = 0){
+function expoente(n1 = 0, n2 = 0) {
     var resultexpoente = n1 ** n2
     return resultexpoente
 }
@@ -102,8 +53,15 @@ exemplo:
 CalculeSomaQuadrado(3) -> 14
 Processamento: 1² + 2² + 3² = 14
 */
-function calculaquadrado (n1){
-    while ( )
+function calculaquadrado(n1) {
+    var resultado = []
+    var soma = 0
+    for (var i = 0; i < n1; i++) {
+        //var total = n1[i] + n1
+        resultado = (resultado[i]) ** 2
+        soma = soma + resultado[i]
+        console.log(`o valor é:  ${soma}`)
+}
 }
 /* Questão 04
 Cria uma função que receba um array e calcule qual a maior sequencia de inteiros que existe nele.
@@ -113,3 +71,4 @@ calculaSequencia([3,4,1,2]) ->2
 calculaSequencia([10,11,12,9,10]) -> 3
 (10->11->12)
 */
+
